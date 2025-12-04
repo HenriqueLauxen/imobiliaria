f8c7262a6243f2b1541f53e563ec624451e2a9d0
Henrique Lauxen <151177254+HenriqueLauxen@users.noreply.github.com>
2025-12-04 08:33:57 -0300

    dtos, models -> architeture

--- src/main/java/atividade_final/imobiliaria/controllers/UserController.java
+++ src/main/java/atividade_final/imobiliaria/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.example projeto.controllers;
+package atividade_final.imobiliaria.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
